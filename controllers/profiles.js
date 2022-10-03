29f97589baab44da2ee046f149d3bc9e788e0137
James Hudson <jmh357@comcast.net>
2022-10-03 13:09:50 -0500

    add profiles show view

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -14,6 +14,24 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  console.log('show function - profiles')
+  Profile.findById(req.params.id)
+  .then(profile => {
+    const isSelf = profile._id.equals(req.user.profile._id)
+    res.render('profiles/show', {
+      title: `${profile.name}'s Profile`,
+      profile,
+      isSelf,
+    })
+  })
+  .catch(err => {
+    console.log(err, 'ERROR')
+    res.redirect('/profiles')
+  })
+}
+
 export {
-  index
+  index,
+  show,
 }
