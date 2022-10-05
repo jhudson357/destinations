210d3130404e653b80ab0fb529e62dbcba4a9415
James Hudson <jmh357@comcast.net>
2022-10-04 20:15:56 -0500

    change dateVisited field to type String

--- models/destination.js
+++ models/destination.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const reviewSchema = new Schema({
-  dateVisited: Date,
+  dateVisited: String,
   rating: Number,
   recommend: Boolean,
   title: String,
